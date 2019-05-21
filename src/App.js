6543b438837f64c54f29b9979eec5a9aea094e14
jeongyip <jeongyip@gmail.com>
2019-05-22 00:20:26 +0900

    code splitting with loadable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import loadable from '@loadable/component'
+// import notify from './notify.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const SplitMe = loadable(() => import('./SplitMe'));
+
+class App extends Component {
+  state = {
+    visible: false,
+  };
+
+  handleClick = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  render() {
+    const { visible } = this.state;
+    return <div>
+      <button onClick={ this.handleClick }>Click Me</button>
+      { visible &&
+          <SplitMe />
+      }
     </div>
-  );
+  }
 }
 
 export default App;
---
+++ src/SplitMe.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SplitMe = () => {
+  return <div>리액트 컴포넌트를 코드스플리팅 하자.</div>;
+};
+
+export default SplitMe;
